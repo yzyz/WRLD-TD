72bc3d12e72219d18c766aad6eb7774e99cf3c58
James Lin <james97lin@gmail.com>
2017-10-07 19:20:44 -0700

    Got a navmesh to spawn in at runtime.

--- Assets/Scripts/ResizeWorld.cs
+++ Assets/Scripts/ResizeWorld.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ResizeWorld : MonoBehaviour {
 
 	void Start () {
-		Invoke ("Resize", 5f);
+		Invoke ("Resize", 2f);
 	}
 	
 	void Resize() {
-		transform.localScale = Vector3.one / 150f;
+        GetComponent<WrldMap>().enabled = false;
+		transform.localScale = Vector3.one / 115f;
+
+        GameObject terrain = transform.Find("Terrain").gameObject;
+
+        GetComponent<NavMeshSurface>().BuildNavMesh();
 	}
 		
 }
