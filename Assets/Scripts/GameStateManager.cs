576e99e4ca4604cc7edd8b59fc05fe17a6f90a61
James Lin <james97lin@gmail.com>
2017-10-08 01:10:36 -0700

    Added money to the menu and script manager. Placing turrets now costs gold.

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GameStateManager : MonoBehaviour {
+
+    public int gold;
+    public int health;
+    public int maxHealth;
+
+    public GameObject leftMenu;
+    public GameObject rightMenu;
+
+	// Use this for initialization
+	void Start () {
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Update gold values
+        leftMenu.transform.Find("CoinObjectTooltip").GetComponent<VRTK_ObjectTooltip>().UpdateText("" + gold);
+        rightMenu.transform.Find("CoinObjectTooltip").GetComponent<VRTK_ObjectTooltip>().UpdateText("" + gold);
+    }
+}
--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -9,11 +9,13 @@ public class ShopItem : VRTK_InteractableObject {
     public static float rotateSpeed = .2f;
 
     public GameObject prefab;
+    public int cost;
+    public GameStateManager manager;
 
     private VRTK_InteractGrab grabObject;
 
 	void Start () {
-        
+        manager = GameObject.Find("Script Manager").GetComponent<GameStateManager>();
     }
 
     public override void StartUsing(VRTK_InteractUse usingObject)
@@ -28,9 +30,15 @@ public class ShopItem : VRTK_InteractableObject {
 
     private void TakeItem(VRTK_InteractUse usingObject)
     {
-        // Spawn new item and have it be grabbed
-        GameObject item = Instantiate(prefab, transform.position - new Vector3(0, .1f, 0), transform.rotation);
-        item.transform.parent = usingObject.gameObject.transform;
-        usingObject.gameObject.GetComponent<ControllerListener>().storedObject = item;
+        if (manager.gold >= cost)
+        {
+            // Spawn new item and have it be grabbed
+            GameObject item = Instantiate(prefab, transform.position - new Vector3(0, .1f, 0), transform.rotation);
+            item.transform.parent = usingObject.gameObject.transform;
+            usingObject.gameObject.GetComponent<ControllerListener>().storedObject = item;
+
+            // Take away money
+            manager.gold -= cost;
+        }
     }
 }
