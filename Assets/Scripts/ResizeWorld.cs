2e5288759dd6ae3b7fe43eb39487155632f32704
alex dai <alexanderdai@gmail.com>
2017-10-07 18:05:34 -0700

    initial commit

---
+++ Assets/Scripts/ResizeWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeWorld : MonoBehaviour {
+
+	void Start () {
+		Invoke ("Resize", 5f);
+	}
+	
+	void Resize() {
+		transform.localScale = Vector3.one / 150f;
+	}
+		
+}
