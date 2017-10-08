77bf75e3c6454cbf7319c760418fa495b02d7758
alex dai <alexanderdai@gmail.com>
2017-10-07 22:13:13 -0700

    monster navigation

---
+++ Assets/monsters/MoveTo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+	public Transform goal;
+
+	void Update() {
+		UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+}
